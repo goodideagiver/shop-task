98f06688bd406749c45ee778a87818f6c2c0c3e7
prplblck <kariurek@gmail.com>
2022-10-22 19:18:48 +0200

    Feat: add debounce hook implementation to search modal

--- src/components/SearchModal/SearchModal.tsx
+++ src/components/SearchModal/SearchModal.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from 'react'
+import { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
+import { useDebounce } from '../../hooks/useDebounce'
 import classes from './SearchModal.module.css'
 import { useIsBrowser } from './useIsBrowser'
 
@@ -8,7 +9,19 @@ type Props = {
 }
 
 export const SearchModal = ({ onClose }: Props) => {
+  const [searchInputValue, setSearchInputValue] = useState('')
   const isBrowser = useIsBrowser()
+
+  const itemName = useDebounce(searchInputValue)
+
+  const searchInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInputValue(event.target.value)
+  }
+
+  useEffect(() => {
+    if (itemName.trim().length === 0) return
+    console.log(itemName)
+  }, [itemName])
 
   if (!isBrowser) return null
 
@@ -18,17 +31,26 @@ export const SearchModal = ({ onClose }: Props) => {
       <div className={classes.root}>
         <div className={classes.content}>
           <header>
-            <label for='search'>Search items</label>
+            <label htmlFor='search'>Search items</label>
             <input
               id='search'
               className={classes['search-input']}
               type='text'
               placeholder='item name'
+              value={searchInputValue}
+              onChange={searchInputHandler}
             />
             <button className={classes.close} onClick={onClose}>
               Close modal
             </button>
           </header>
+          <main>
+            {itemName.trim().length === 0 && (
+              <div className={classes['no-items']}>
+                <p>Type anything in search field to find items</p>
+              </div>
+            )}
+          </main>
         </div>
       </div>
     </>,
